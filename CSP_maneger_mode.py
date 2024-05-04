717b69f5f725f598e3f9a73c6e1c484246761574
alphaprime0 <grothendieck_motif@outlook.com>
2024-05-04 19:14:27 -0400

    Update CSP_maneger_mode.py

--- CSP_maneger_mode.py
+++ CSP_maneger_mode.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 #coding:utf-8
-import pandas as pd
-
-
-
 
 
 def backtracking(row, candidates, requested_positions, players, budget):
@@ -12,12 +8,12 @@ def backtracking(row, candidates, requested_positions, players, budget):
 
     else:
         for i in range(row, len(candidates)):
-            player = candidates[row].player
+            player = candidates[row]
             if Is_valid(player):
                 players.append(player)
-                if backtracking(row + 1, candidates[1:], requested_positions[1:], players, budget - player.price):
+                if backtracking(row + 1, candidates[1:], requested_positions[1:], players, budget - player['price']):
                     return True
-                
+                players.pop()               
     return False
 
 def Is_valid(player): # Done psudo code check validation
@@ -27,20 +23,20 @@ def Is_valid(player): # Done psudo code check validation
 
     # the constraint
     # financial constraints
-    if (Not financial_contraint): # financial constraint failed
+    if budget < player['price'] : # financial constraint failed
         return False
     
     # Position constraints
-    if (Not Position_contraint):
+    if requested_positions[0] != player['position']:
         return False
     
-    # Nationality constraints
-    if (Not nationality_contraint): 
-        return False
+    # Nationality constraints # TODO
+    #if (Not nationality_contraint): 
+    #    return False
     
-    # all other constraints
-    if (Not other_contraint): 
-        return False
+    # all other constraints # TODO
+    #if (Not other_contraint): 
+    #    return False
          
     return True
     
@@ -49,21 +45,40 @@ def find_all_players(requested_positions, players): # done psudo code finish con
     # check if find all positions
     if requested_positions == players.positions:
         return True
+    
+def print_output(output):
+    for item in output:
+        print('id:', item['id'],
+              'name:', item['name'], 
+              'position:', item['position'], 
+              'price:', item['price'], 
+              'nationality:', item['nationality'])
 
 if __name__ == '__main__':
     
+    import pandas as pd
 
-    candidates = pd.read_csv('available_players.csv') 
-    candidates = candidates(['name', 'position', 'price'])
-    requested_positions = pd.read_csv('requested_players.csv')
-    requested_positions = requested_positions(['position'])
-    
+    ## load candidates   
+    df = pd.read_csv('data/transfermarkt_data.csv')
+    # pick useful information
+    df = df[['player_id', 'name', 'sub_position', 'market_value_in_eur', 'country_of_birth']]
+    # rename columns
+    df.columns=['id', 'name', 'position', 'price', 'nationality']
+    # change format from dataframe to list of dictionary
+    candidates = candidates = df.to_dict(orient='records')
 
+    ## budget and requeted_position. We can change it to input file later
+    # The following is just for testing now
+    budget = 10000000
+    requested_positions = ['Goalkeeper', 'Centre-Back', 'Centre-Back', 'Attacking Midfield']
+
+
+    # initial condition
     players = []
     row = 0
-    budget = input_buget
+    
 
     backtracking(row, candidates, requested_positions, players)
 
-    print(players)
+    print_output(players)
     
