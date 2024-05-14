5a9ae3a7721f5cbde3de79be05d66f8ce922015d
Giacomo Radaelli <gradaelli@sandiego.edu>
2024-05-14 00:37:57 -0400

    webapp

--- webapp.py
+++ webapp.py
@@ -1,72 +1,119 @@
 import streamlit as st
 
+# Initialize session state
+if 'player_count' not in st.session_state:
+    st.session_state.player_count = 0
+
+# Initialize lists to store player selections
+if 'player_positions' not in st.session_state:
+    st.session_state.player_positions = {}
+if 'player_traits' not in st.session_state:
+    st.session_state.player_traits = {}
+
 def main():
-    st.set_page_config(page_title="European Soccer Trade Machine")
 
-    st.markdown(
-        """
-        <style>
-            body {
-                margin: 0;
-                font-family: Arial, sans-serif;
-                background-color: #f9f9f9;
-                color: rgb(32, 8, 62);
-            }
-            header {
-                background-color: #2c9dba;
-                color: #f9f9f9;
-                padding: 20px 0;
-                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-                text-align: center;
-            }
-            .container {
-                max-width: 700px;
-                padding: 20px;
-                border: 1px solid #070707; /* Add a border */
-            }
-            .btn {
-                background-color: #2c9dba;
-                color: #f9f9f9;
-                padding: 10px 20px;
-                border: none;
-                border-radius: 5px;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 16px;
-                margin: 10px;
-                cursor: pointer;
-                transition: background-color 0.3s;
-            }
-            .btn:hover {
-                background-color: #45a049;
-            }
-            .dropdown select {
-                padding: 10px;
-                font-size: 16px;
-            }
-            .budget {
-                margin-top: 10px; /* Add space between team selection dropdown and transfer budget textbox */
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+    # Set wide mode for the entire page
+    st.set_page_config(layout="wide")
 
-    st.header("European Soccer Trade Machine")
+    # Divide layout into two columns
+    colF, colS, colT= st.columns([1, 2, 1])
+    with colS:
+        # Set title of the app
+        st.title("European Soccer Trade Machine")
 
-    st.markdown(
-        """
-        <div class="container">
-            <!-- Introduction -->
-            <section id="introduction">
-                <h2>Welcome to the European Soccer Trade Machine!</h2>
-                <p>Your go-to resource for making informed transfer decisions in the highly competitive world of professional soccer. Our platform offers a comprehensive toolset designed to assist general managers and front office staff in navigating the complexities of player trades across various European leagues.</p>
-                <!-- Add rest of your HTML content here -->
-            </section>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+
+    # Divide layout into two columns
+    colA, colB,colC= st.columns([1, 11, 1])
+
+    with colB:
+        # Add introduction section
+        st.write("Welcome to the European Soccer Trade Machine! Your go-to resource for making informed transfer decisions in the highly competitive world of professional soccer. Our platform offers a comprehensive toolset designed to assist general managers and front office staff in navigating the complexities of player trades across various European leagues.")
+
+        st.subheader("Here's what you need to do:")
+        st.markdown("- **Select Your Team:** Begin by choosing your team of interest from a wide array of European leagues, including the Premier League, Serie A, La Liga, and more. First, pick the league in which your desired team competes, then select the team itself.")
+        st.markdown("- **Set Your Budget:** Determine your team's financial resources by entering a transfer budget in Euros. This budget will serve as the foundation for your transfer decisions, ensuring that you stay within your financial constraints while pursuing strategic player acquisitions.")
+        st.markdown("- **Specify Player Positions and Traits (Optional):** Tailor your search further by specifying player positions and traits that align with your team's needs and strategic objectives. You can input up to five position-player trait pairings to refine your transfer targets.")
+        st.markdown("- **Enter and Evaluate:** Once you've provided all necessary information, click the 'Enter' button to proceed. Our platform will process your inputs and generate recommendations based on factors such as positional needs, financial constraints, and season ambitions.")
+
+        st.write("By leveraging our tool's advanced capabilities, including machine learning algorithms and constraint satisfaction programming (CSP) techniques, you'll gain valuable insights into potential transfers that can enhance your team's competitiveness and success on the field.")
+
+    # Divide layout into two columns
+    col1, col2, col3, col4= st.columns([1,3, 3,1])
+
+    # Select Your Team subsection
+    with col2:
+        # Select Your Team subsection
+        st.header("Select Your Team")
+
+        # Select League dropdown
+        selected_league = st.selectbox("Select League", ["Select a League", "Premier League", 
+                                                        "Serie A", "La Liga", "Ligue 1", 
+                                                        "Bundesliga", "Eredivisie", "Liga Nos", 
+                                                        "Jupiler Pro League", "Premier Liga", 
+                                                        "Scottish Premiership"])
+
+        if selected_league != "Select a League":
+            # Dictionary of teams for each league
+            teams = {
+                "Premier League": ['Arsenal', 'Aston Villa', 'Bournemouth', 'Brighton', 'Chelsea', 'Crystal Palace', 'Everton', 'Fulham', 'Leeds United', 'Leicester City', 'Liverpool', 'Manchester City', 'Manchester Utd', 'Newcastle Utd', "Nott'ham Forest", 'Southampton', 'Tottenham', 'West Ham', 'Wolves'],
+                "Serie A": ['Atalanta', 'Bologna', 'Cremonese', 'Empoli', 'Fiorentina', 'Hellas Verona', 'Inter', 'Juventus', 'Lazio', 'Lecce', 'Milan', 'Monza', 'Napoli', 'Roma', 'Salernitana', 'Sampdoria', 'Sassuolo', 'Spezia', 'Torino', 'Udinese'],
+                "La Liga": ['Almería', 'Athletic Club', 'Atlético Madrid', 'Barcelona', 'Betis', 'Cádiz', 'Celta Vigo', 'Espanyol', 'Getafe', 'Girona', 'Mallorca', 'Osasuna', 'Rayo Vallecano', 'Real Madrid', 'Real Sociedad', 'Sevilla', 'Valencia', 'Valladolid', 'Villarreal', 'Elche'],
+                "Ligue 1": ['Ajaccio', 'Angers', 'Auxerre', 'Brest', 'Clermont Foot', 'Lens', 'Lille', 'Lorient', 'Lyon', 'Marseille', 'Metz', 'Monaco', 'Montpellier', 'Nantes', 'Nice', 'Paris S-G', 'Rennes', 'Reims', 'Strasbourg', 'Troyes'],
+                "Bundesliga": ['Augsburg', 'Bayern Munich', 'Bochum', 'Dortmund', 'Eintracht Frankfurt', 'Freiburg', 'Hertha BSC', 'Hoffenheim', 'Köln', 'Mainz 05', 'RB Leipzig', "M'Gladbach", 'Schalke 04', 'Stuttgart', 'Union Berlin', 'Wolfsburg', 'Werder Bremen'],
+                "Eredivisie": ['Ajax', 'AZ Alkmaar', 'Cambuur', 'Emmen', 'Excelsior', 'Feyenoord', 'Fortuna Sittard', 'Go Ahead Eagles', 'Groningen', 'Heerenveen', 'NEC Nijmegen', 'PSV Eindhoven', 'RKC Waalwijk', 'Sparta Rotterdam', 'Utrecht', 'Vitesse', 'Volendam', 'Willem II', 'Zwolle'],
+                "Liga Nos": ['Arouca', 'Belenenses', 'Benfica', 'Boavista', 'Braga', 'Estoril', 'Famalicão', 'Gil Vicente', 'Marítimo', 'Moreirense', 'Paços de Ferreira', 'Portimonense', 'Porto', 'Rio Ave', 'Santa Clara', 'Sporting CP', 'Tondela', 'Vizela', 'Vitória Guimarães', 'Vitória SC'],
+                "Jupiler Pro League": ['Anderlecht', 'Antwerp', 'Beerschot VA', 'Cercle Brugge', 'Charleroi', 'Club Brugge', 'Eupen', 'Genk', 'Gent', 'Kortrijk', 'Mechelen', 'Oostende', 'RSC Anderlecht', 'Sint-Truiden', 'Sporting Charleroi', 'Standard Liège', 'STVV', 'Waasland-Beveren', 'Zulte Waregem'],
+                "Premier Liga": ['Akhmat Grozny', 'Arsenal Tula', 'CSKA Moscow', 'Dinamo Moscow', 'Dynamo Moscow', 'FC Krasnodar', 'Khimki', 'Lokomotiv Moscow', 'Rubin Kazan', 'Rostov', 'SKA-Khabarovsk', 'Spartak Moscow', 'Ufa', 'Ural', 'Veles Moscow', 'Zenit St. Petersburg'],
+                "Scottish Premiership": ['Aberdeen', 'Celtic', 'Dundee', 'Dundee Utd', 'Hamilton', 'Hearts', 'Hibernian', 'Livingston', 'Motherwell', 'Partick', 'Rangers', 'Ross County', 'St Johnstone', 'St Mirren']
+            }
+
+            # Select team dropdown based on selected league
+            selected_team = st.selectbox(f"Select Team for {selected_league}", ["Select a Team"] + sorted(teams[selected_league]))
+
+            # Budget textbox
+            transfer_budget = st.number_input("Transfer Budget (in Euros):", min_value=0, step=1)
+
+           # Player positions
+            st.header("Player Positions")
+            positions = [
+                'Select Position','Defensive Midfield', 'Centre-Forward', 'Centre-Back', 'Right-Back', 'Left-Back',
+                'Attacking Midfield', 'Right Winger', 'Right Midfield', 'Second Striker',
+                'Central Midfield', 'Left Winger', 'Left Midfield', 'Goalkeeper'
+            ]
+            traits = {
+                'Select Position': ['None'],
+                'Defensive Midfield': ['None', 'Playmaker'],
+                'Centre-Forward': ['None','Free-kick Specialist', 'Sharp-shooter'],
+                'Centre-Back': ['None'],
+                'Right-Back': ['None','Free-kick Specialist'],
+                'Left-Back': ['None','Free-kick Specialist'],
+                'Attacking Midfield': ['None','Free-kick Specialist', 'Playmaker'],
+                'Right Winger': ['None','Free-kick Specialist'],
+                'Right Midfield': ['None','Free-kick Specialist'],
+                'Second Striker': ['None','Free-kick Specialist', 'Sharp-shooter'],
+                'Central Midfield': ['None','Free-kick Specialist', 'Playmaker'],
+                'Left Winger': ['None','Free-kick Specialist'],
+                'Left Midfield': ['None','Free-kick Specialist'],
+                'Goalkeeper': ['None']
+            }
+            if st.session_state.player_count < 5:
+                if st.button("+ Add Player"):
+                    st.session_state.player_count += 1
+            for i in range(st.session_state.player_count):
+                player_position = st.selectbox(f"Player {i+1} Position:", positions)
+                player_trait = st.selectbox(f"Player {i+1} Trait:", traits[player_position])
+                if player_position and player_trait: 
+                    st.session_state.player_positions[i+1] = player_position
+                    st.session_state.player_traits[i+1] = player_trait 
+                    
+            # # Print out the lists for testing
+            # st.write("Player Positions:", st.session_state.player_positions)
+            # st.write("Player Traits:", st.session_state.player_traits)
+
+    with col3:
+        st.header("Output")
+        # Placeholder for output
+        st.write("Output will be displayed here")
 
 if __name__ == "__main__":
     main()
