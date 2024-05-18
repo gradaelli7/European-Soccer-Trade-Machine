ebc88654a382f84aca1f66ee81e01af18d72587e
Giacomo Radaelli <gradaelli@sandiego.edu>
2024-05-17 20:33:18 -0400

    formatting budget

--- webapp.py
+++ webapp.py
@@ -91,7 +91,7 @@ def main():
             formatted_budget = f"€{transfer_budget:,.2f}"
 
             # Display the formatted transfer budget
-            st.write(f"{formatted_budget}")
+            st.write(f"Budget: {formatted_budget}")
 
            # Player positions
             positions = [
